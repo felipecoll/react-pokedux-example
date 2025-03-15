1665ce3c777df8bf6dc5bab331fb9490522b7ca2
Felipe Coll <leonfelipecoll@gmail.com>
2025-03-15 16:42:49 -0300

    Estructura de visualizacion de objetos

--- src/components/Pokecard.jsx
+++ src/components/Pokecard.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import { Card } from 'antd';
 import Meta from 'antd/lib/card/Meta';
+import { StarOutlined } from '@ant-design/icons';
 
 const Pokecard = () => {
   return (
    <>
         <Card 
-            style={{width:250}}
             title='Ditto'
             cover={<img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/132.png' alt='Ditto' />}>
+             extra={<StarOutlined/>} 
             <Meta description='fire, magic'/>
         </Card>
         
--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -1,18 +1,24 @@
-import React from 'react'
-import Pokecard from './Pokecard'
+import Pokecard from "./Pokecard";
+import "./PokemonList.css";
 
 const PokemonList = ({ pokemons }) => {
     return (
-      <div className='PokemonList'>
-        {pokemons.map((pokemon) => {
-          return <Pokecard />;
-        })}
+      <div className="PokemonList">
+        {pokemons && pokemons.length > 0 ? (
+          pokemons.map((pokemon, index) =>
+            pokemon ? (
+              <Pokecard key={pokemon.id || index} pokemon={pokemon} />
+            ) : null
+          )
+        ) : (
+          <p>No hay Pokémon disponibles</p>
+        )}
       </div>
     );
   };
-  
+
   PokemonList.defaultProps = {
-    pokemons: Array(10).fill(''),
+    pokemons: Array(10).fill({ id: Math.random(), name: "Unknown" }),
   };
-  
-  export default PokemonList
+
+export default PokemonList
